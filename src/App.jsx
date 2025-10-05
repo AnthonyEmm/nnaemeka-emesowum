aa18627bed32e70994a06ffe1f56ac2ae06a21cf
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2025-10-05 09:25:47 +0200

    Added a scroll to top function and button in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -8,8 +8,24 @@ import About from "./components/About";
 import { BrowserRouter } from "react-router-dom";
 import Services from "./components/Services";
 import TechnewsCarousel from "./components/TechnewsCarousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [showScroll, setShowScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScroll(window.scrollY > 200);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <BrowserRouter>
@@ -26,6 +42,18 @@ function App() {
         </div>
         <div>
           <Footer />
+          {/* Scroll to top button */}
+          {showScroll && (
+            <button
+              onClick={scrollToTop}
+              className="fixed bottom-8 right-8 z-50 bg-gray-800 text-white p-2 border rounded-full
+              shadow-lg hover:bg-gray-700 transition"
+              aria-label="Scroll to top"
+              title="Scroll to top"
+            >
+              <FontAwesomeIcon icon={faArrowUp} size="lg" />
+            </button>
+          )}
         </div>
       </BrowserRouter>
     </>
